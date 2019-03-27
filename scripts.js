604a70c5931b5f2f622ac10ff3688eb5160d83f9
karma <karmadrukpa11@gmail.com>
2019-03-27 13:02:39 -0700

    update products images

--- scripts.js
+++ scripts.js
@@ -1,15 +1,47 @@
-$(document).ready(function () {
+$(document).ready(function() {
 	$('.sidenav').sidenav();
 	$('.MyModal').openModal();
+
+	$('#mc-embedded-subscribe-form').on('submit', event => {
+		event.preventDefault();
+		let data = $('#mc-embedded-subscribe-form').serializeArray();
+		console.log(data);
+		let firstName = data[0].value;
+
+		let regex = /^[a-zA-Z]+$/;
+
+		if (!regex.test(firstName)) {
+			alert('please put your firstName');
+			return;
+		}
+	});
 });
 
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.tap-target');
 	var instances = M.TapTarget.init(elems, options);
 
 	instance.next();
 	instance.next(3); // Move next n times.
 	instance.close();
+});
 
+// ! modal
+$(document).ready(function() {
+	$('.sidenav').sidenav();
+	$('.modal').modal();
+	$('.modal').modal({
+		dismissible: false
+	});
+	$('.signing-up').fadeOut();
+	$('.modal').modal('open');
+	$('.signing-up-later').click(() => {
+		$('.signing-up').fadeIn('slow');
+	});
+	$('.signing-up').click(() => {
+		$('.signing-up').fadeOut('slow');
+	});
+	$('.send').click(() => {
+		//
+	});
 });
